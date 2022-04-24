f6aaa77b9eda3d6a090c98bcc6e684bfcdfabbc9
Sadot Sanchez <sadotdsm@gmail.com>
2022-04-23 18:44:54 -0700

    Add navigation buttons and basic modal

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-// const modal = document.querySelector("#modal");
-// const openModal = document.querySelector(".open-button");
-// const closeModal = document.querySelector(".close-button");
-
-// openModal.addEventListener("click", () => {
-//     modal.showModal();
-// });
-
-// closeModal.addEventListener("click", () => {
-//     modal.close();
-// });
-
 const pokemonList = document.querySelector(".pokemon-list");
+
+const previousPage = document.querySelector("#previousPage");
+const nextPage = document.querySelector("#nextPage");
+
+let offset = 1;
+let limit = 19;
+
+previousPage.addEventListener("click", () => {
+    if (offset != 1) {
+        offset -= 20;
+        removeChildNodes(pokemonList);
+        fetchPokemons(offset, limit);
+    }
+});
+
+nextPage.addEventListener("click", () => {
+    offset += 20;
+    removeChildNodes(pokemonList);
+    fetchPokemons(offset, limit);
+});
 
 function fetchPokemon(id) {
     fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
@@ -20,14 +28,38 @@ function fetchPokemon(id) {
         });
 }
 
-function fetchPokemons(number) {
-    for (let i = 1; i <= number; i++) {
+function fetchPokemons(offset, limit) {
+    for (let i = offset; i <= offset + limit; i++) {
         fetchPokemon(i);
     }
 }
 
-function pokemonNameCapitalize(name) {
+function capitalizePokemonName(name) {
     return (capitalizedName = name.charAt(0).toUpperCase() + name.slice(1));
+}
+
+function addModalInfo(pokemon) {
+    //
+    // const closeModal = document.querySelector(".close-button");
+    const modal = document.createElement("dialog");
+    modal.classList.add("modal");
+    modal.id = "modal";
+
+    const closeContainer = document.createElement("div");
+
+    const closeButton = document.createElement("button");
+    closeButton.textContent = "Close";
+    closeButton.addEventListener("click", () => {
+        modal.close();
+    });
+    closeContainer.appendChild(closeButton);
+    modal.appendChild(closeContainer);
+
+    const pokemonImg = document.createElement("img");
+    pokemonImg.src = pokemon.sprites.front_default;
+
+    const body = document.querySelector("body");
+    body.appendChild(modal);
 }
 
 function createCard(pokemon) {
@@ -42,18 +74,32 @@ function createCard(pokemon) {
 
     const pokemonImg = document.createElement("img");
     pokemonImg.src = pokemon.sprites.front_default;
+    pokemonImg.alt = pokemon.name;
     pokemonImg.loading = "lazy";
 
     pokemonImgContainer.appendChild(pokemonImg);
 
     const pokemonName = document.createElement("p");
-    pokemonName.textContent = `${pokemonNameCapitalize(pokemon.name)}`;
+    pokemonName.textContent = capitalizePokemonName(pokemon.name);
 
     pokemonButton.appendChild(pokemonImgContainer);
     pokemonButton.appendChild(pokemonName);
+    pokemonButton.id = pokemon.id;
+    pokemonButton.addEventListener("click", () => {
+        addModalInfo(pokemon);
+        const modal = document.querySelector("#modal");
+        modal.showModal();
+    });
+
     pokemonCard.appendChild(pokemonButton);
 
     pokemonList.appendChild(pokemonCard);
 }
 
-fetchPokemons(24);
+function removeChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+fetchPokemons(offset, limit);
